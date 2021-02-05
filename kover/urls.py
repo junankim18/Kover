bf7c7a240814a2de7b75a37601e5c18e4deb5cf9
Junan Kim <junankim18@naver.com>
2021-02-06 00:36:36 +0900

    url 수정

--- kover/urls.py
+++ kover/urls.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 app_name = 'kover'
 
 urlpatterns = [
-    # name은 url이름과 최대한 같게
-    # path('/'  # 메인 화면),
-    # path('profile_block/'  # 개인 프로필-block),
-    # path('profile_geo/'  # 개인 프로필-geo),
-    # path('settings/'  # 설정),
-    # path('feed/play_inf'  # 연극-정보),
-    # path('feed/play_lib'  # 연극-자유),
-    # path('feed/musical_inf'  # 뮤지컬-정보),
-    # path('feed/musical_lib'  # 뮤지컬-자유),
-    # path('feed/'  # 피드-메인),
-    # path('feed/question'  # 질문),
-    # path('contents/<int:pk>/'  # 공연별 디테일),
-    # path('contents/<int:pk>/comment'  # 공연별 댓글),
-    # path('contents/<int:pk>/review'  # 공연별 리뷰),
-    # path('contents/'  # 공연 평가할 수 있는 페이지-작품들의 리스트가 뜬다)
-    # path('place/<int:pk>/'  # 공연장 디테일),
-    # path('search/'  # 검색했을때),
+    path('/', views, name='main'),  # 메인 화면)
+    path('profile_block/', views, name=''),  # 개인 프로필-block),
+    path('profile_geo/', views, name=''),  # 개인 프로필-geo),
+    path('settings/', views, name=''),  # 설정),
+    path('feed/play_inf', views, name=''),  # 연극-정보),
+    path('feed/play_lib', views, name=''),  # 연극-자유),
+    path('feed/musical_inf', views, name=''),  # 뮤지컬-정보),
+    path('feed/musical_lib', views, name=''),  # 뮤지컬-자유),
+    path('feed/', views, name=''),  # 피드-메인),
+    path('feed/question', views,  name=''),  # 질문),
+    path('contents/<int:pk>/', views, name=''),  # 공연별 디테일),
+    path('contents/<int:pk>/comment', views, name=''),  # 공연별 댓글),
+    path('contents/<int:pk>/review', views, name=''),  # 공연별 리뷰),
+    path('contents/', views, name=''),  # 공연 평가할 수 있는 페이지-작품들의 리스트가 뜬다)
+    path('place/<int:pk>/', views, name=''),  # 공연장 디테일),
+    path('search/', views, name=''),  # 검색했을때),
 ]
