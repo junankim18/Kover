15585898ddbdfe0995e04f30e0d16a9efbe09c71
Junan Kim <junankim18@naver.com>
2021-02-07 20:28:02 +0900

    hall overlap issue solved

--- kover/views.py
+++ kover/views.py
@@ -9,11 +9,37 @@ def profile_block(request):
     actors = user.like_actor.all()
     favorites = user.interested_show.all()
     reviews = user.review_author.all().order_by('-id')
+    showlist = list(shows)
+    hallnum = len(showlist)
+    wantlist = []
+    print(showlist)
+    print(showlist[0].show_hall)
+    print(showlist[1].show_hall)
+    print(showlist[2].show_hall)
+    if showlist[0].show_hall == showlist[2].show_hall:
+        print('같다')
+
+    for i in range(hallnum):
+        for j in range(hallnum):
+            if i >= j:
+                pass
+            elif showlist[i].show_hall == showlist[j].show_hall:
+                pass
+            else:
+                wantlist.append(showlist[i])
+
+    overlapnum = len(wantlist)
+
     ctx = {
         'user': user,
         'shows': shows,
         'actors': actors,
         'favorites': favorites,
-        'reviews': reviews
+        'reviews': reviews,
+        'showlist': showlist,
+        'hallnum': hallnum,
+        'overlapnum': overlapnum,
+        'wantlist': wantlist,
+
     }
     return render(request, 'kover/profile_block.html', ctx)
