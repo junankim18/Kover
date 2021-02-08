5635de404088d2bac0fe973e5c1b26be7ce15da6
seohyun319 <tjguselly0319@gmail.com>
2021-02-08 23:38:53 +0900

    html 추가

--- kover/urls.py
+++ kover/urls.py
@@ -10,12 +10,16 @@ urlpatterns = [
          name='profile_block'),  # 개인 프로필-block),
     # path('profile_geo/', views, name=''),  # 개인 프로필-geo),
     # path('settings/', views, name=''),  # 설정),
+
+
     # path('feed/play_inf', views, name=''),  # 연극-정보),
     # path('feed/play_lib', views, name=''),  # 연극-자유),
     # path('feed/musical_inf', views, name=''),  # 뮤지컬-정보),
     # path('feed/musical_lib', views, name=''),  # 뮤지컬-자유),
-    # path('feed/', views, name=''),  # 피드-메인),
+    path('feed/', views.Feed.as_view(), name='feed_main'),  # 피드-메인),
     # path('feed/question', views,  name=''),  # 질문),
+
+
     # path('contents/<int:pk>/', views, name=''),  # 공연별 디테일),
     # path('contents/<int:pk>/comment', views, name=''),  # 공연별 댓글),
     # path('contents/<int:pk>/review', views, name=''),  # 공연별 리뷰),
--- kover/views.py
+++ kover/views.py
@@ -1,6 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 from .models import Hall, User, Show, People, Review, Feed_post, Feed_comment, Time, Profile
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
 
 def profile_block(request):
     show = Show.objects.all()
@@ -10,3 +14,32 @@ def profile_block(request):
         'show': show
     }
     return render(request, 'kover/profile_block.html', ctx)
+
+
+
+
+class Feed(View):
+    template_name = 'kover/feed_layout.html'
+    
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Feed, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        feed_list = Feed_post.objects.all()
+        ctx = {"feeds": feed_list}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request):
+        request = json.loads(request.body)
+        feed_id = request['id']
+        button_type = request['type']
+        feed = Feed_post.objects.get(id=feed_id)
+        if button_type == feed_like:
+            feed_like = feed_like + 1
+        Feed_post.save()
+
+        return JsonResponse({'id': feed_id, 'type':feed_like})
+
+
