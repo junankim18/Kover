69acf67e6b88462d81030976e4ab4e03cf33dc69
seohyun319 <tjguselly0319@gmail.com>
2021-02-18 21:09:28 +0900

    feed_board add

--- kover/views.py
+++ kover/views.py
@@ -145,45 +145,51 @@ def feed_main(request):
     return render(request, 'kover/feed_main.html', ctx)
 
 
-#def feed_page(request, pk):
-def feed_page(request):
-    feeds= Feed_post.objects.all()
-    #feeds = Feed_post.objects.get(id=pk)
+# def feed_page(request):
+def feed_page(request, pk):
+    
+    # feeds= Feed_post.objects.all()
+    feed = Feed_post.objects.get(pk=pk)
+  
     comlist = []
-    for feed in feeds:
-        comlist.append(len(feed.comment_post.all()))
+    # for feed in feeds:
+    comlist.append(len(feed.comment_post.all()))
     ctx = {
-        'feeds': feeds,
+        'feed': feed,
         'comlist': comlist
     }
     return render(request, 'kover/feed_page.html', ctx)
 
 
 def feed_musical_lib(request):
-    
-    return render(request, 'kover/feed_musical_lib.html')
-
-def feed_musical_inf(request):
-    # username = Profile.objects.filter(id=request.user.id)
-    # if username:
-    #     actors = username[0].like_actor.all().order_by('people_name')
-    # else:
-    #     actors = []
-
-    # feed= Feed_post.objects.all()
-    # feed = Feed_post.objects.get(pk=pk)
-    feed = Feed_post.objects.filter(feed_type='musical_inf').order_by(
+    feed = Feed_post.objects.filter(feed_type='musical_lib')
+    feeds = Feed_post.objects.filter(feed_type='musical_lib').order_by(
         '-feed_created_at')[:]  # 피드 최신 순
     comlist = []
-    # for feed in feeds:
-    #     comlist.append(len(feed.comment_post.all()))
-
+    for feed in feeds:
+        comlist.append(len(feed.comment_post.all()))
     ctx = {
         'feed': feed,
-        # 'feeds': feeds,
+        'feeds': feeds,
         'comlist': comlist,
     }
-    return render(request, 'kover/feed_musical_inf.html')
+    return render(request, 'kover/feed_board.html', ctx)
+
+def feed_musical_inf(request):
+    # feed= Feed_post.objects.all()
+    # feed = Feed_post.objects.get(pk=pk)
+    feed = Feed_post.objects.filter(feed_type='musical_inf')
+    feeds = Feed_post.objects.filter(feed_type='musical_inf').order_by(
+        '-feed_created_at')[:]  # 피드 최신 순
+    comlist = []
+    for feed in feeds:
+        comlist.append(len(feed.comment_post.all()))
+    ctx = {
+        'feed': feed,
+        'feeds': feeds,
+        'comlist': comlist,
+    }
+    return render(request, 'kover/feed_board.html', ctx)
 
 
 def show_detail(request, pk):
