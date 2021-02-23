b234c5ec5728b5f32f3390f80e01eee88b69a4e8
Kim Seohyun <kshjessica@ewhain.net>
2021-02-23 15:40:27 +0900

    static 경로 설정 최종

--- config/settings.py
+++ config/settings.py
@@ -153,10 +153,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-print(f'{BASE_DIR}')
-print(f'{STATIC_URL}')
-print(f'{STATIC_ROOT}')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'config', 'static'),
 ]
